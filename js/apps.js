e33b3de38b9bcb07d98057c96c747bed99b062f2
ian.burnside89@gmail.com <YOUR EMAIL ADDRESSIan>
2018-02-18 15:50:52 -0700

    more additions

--- js/apps.js
+++ js/apps.js
@@ -4,11 +4,12 @@ document.addEventListener("DOMContentLoaded", function() {
 var paletteBox = document.getElementsByClassName('palette')[0]
 var canvas = document.getElementsByClassName('canvas')[0]
 var color = null;
-canvas.addEventListener('click', function(event){
-  event.target.classList.add(color)
-});
+
 paletteBox.addEventListener('click', function(event){
 color = event.target.classList[0];
 })
+canvas.addEventListener('click', function(event){
+  event.target.classList.add(color)
+});
 
   });
