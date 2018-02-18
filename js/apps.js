6447ed3809756a4c4b3f5a5aa7438e0d4cd0dc57
ian.burnside89@gmail.com <YOUR EMAIL ADDRESSIan>
2018-02-18 15:38:41 -0700

    more js rules, added new colors and borders

--- js/apps.js
+++ js/apps.js
@@ -1,6 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("DOM fully loaded and parsed");
 
-var paletteBox = document.getElementsByClassName('palette')
+var paletteBox = document.getElementsByClassName('palette')[0]
+var canvas = document.getElementsByClassName('canvas')[0]
+var color = null;
+canvas.addEventListener('click', function(event){
+  event.target.classList.add(color)
+});
+paletteBox.addEventListener('click', function(event){
+color = event.target.classList[0];
+})
 
   });
